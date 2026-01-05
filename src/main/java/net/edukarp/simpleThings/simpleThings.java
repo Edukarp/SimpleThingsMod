3ae6538c826406fa6bdaecda738d2b38f76db29b
Edukarp <edukarp10@gmail.com>
2026-01-05 17:49:42 -0300

    feat: add Ruby and Ruby Sword

--- src/main/java/net/edukarp/simpleThings/simpleThings.java
+++ src/main/java/net/edukarp/simpleThings/simpleThings.java
@@ -1,6 +1,9 @@
 package net.edukarp.simpleThings;
 
 import com.mojang.logging.LogUtils;
+import net.edukarp.simpleThings.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -20,7 +23,7 @@ import org.slf4j.Logger;
 public class simpleThings
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "simplethings1";
+    public static final String MOD_ID = "simplethings";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -34,6 +37,8 @@ public class simpleThings
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -46,8 +51,13 @@ public class simpleThings
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.RUBY);
+        }
+        if(event.getTabKey() == CreativeModeTabs.COMBAT){
+            event.accept(ModItems.RUBY_SWORD);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
